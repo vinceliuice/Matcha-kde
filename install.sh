034866c199208a8a32fa56059e56ccdd3ae262ba
vinceliuice <vinceliuice@hotmail.com>
2019-07-02 14:46:40 +0800

    update

--- install.sh
+++ install.sh
@@ -17,12 +17,12 @@ install() {
   local AURORAE_THEME=${AURPRAE_DIR}/${name}
   local PLASMA_THEME=${PLASMA_DIR}/${name}
   local LOOKFEEL_THEME=${LOOKFEEL_DIR}/com.github.vinceliuice.${name}
-  local KVANTUM_THEME=${KVANTUM_DIR}/${name}
 
   [[ -d ${AURORAE_THEME} ]] && rm -rf ${AURORAE_THEME}
   [[ -d ${PLASMA_THEME} ]] && rm -rf ${PLASMA_THEME}
   [[ -d ${LOOKFEEL_THEME} ]] && rm -rf ${LOOKFEEL_THEME}
-  [[ -d ${KVANTUM_THEME} ]] && rm -rf ${KVANTUM_THEME}
+  [[ -d ${KVANTUM_THEME}/${name} ]] && rm -rf ${KVANTUM_THEME}/${name}
+  [[ -d ${KVANTUM_THEME}/${name}-dark ]] && rm -rf ${KVANTUM_THEME}/${name}-dark
   [[ -d ${WALLPAPER_DIR}/Matcha ]] && rm -rf ${WALLPAPER_DIR}/Mathca/*.jpg
   [[ -d ${SCHEMES_DIR}/MatchaSea.colors ]] && rm -rf ${SCHEMES_DIR}/MatchaSea.colors
   [[ -d ${SCHEMES_DIR}/MatchaSeaDark.colors ]] && rm -rf ${SCHEMES_DIR}/MatchaSeaDark.colors
@@ -33,11 +33,11 @@ install() {
   mkdir -p                                                                           ${LOOKFEEL_DIR}
   mkdir -p                                                                           ${KVANTUM_DIR}
   mkdir -p                                                                           ${WALLPAPER_DIR}/Matcha
-  
+
   cp -ur ${SRC_DIR}/aurorae/${name}                                                  ${AURORAE_DIR}
   cp -ur ${SRC_DIR}/color-schemes/*.colors                                           ${SCHEMES_DIR}
   cp -ur ${SRC_DIR}/wallpaper/*.jpg                                                  ${WALLPAPER_DIR}/Matcha
-  cp -ur ${SRC_DIR}/Kvantum/${name}                                                  ${KVANTUM_DIR}
+  cp -ur ${SRC_DIR}/Kvantum/*                                                        ${KVANTUM_DIR}
   cp -ur ${SRC_DIR}/plasma/desktoptheme/${name}${ELSE_DARK}                          ${PLASMA_DIR}
   cp -ur ${SRC_DIR}/color-schemes/MatchaSeaDark.colors                               ${PLASMA_DIR}/${name}/colors
   cp -ur ${SRC_DIR}/plasma/look-and-feel/com.github.vinceliuice.${name}              ${LOOKFEEL_DIR}
